1eb22014fb4b06f135fc2b3f09dfeb5664701fce
Jason Würtz <jwuertz@upei.ca>
2018-01-18 10:52:27 -0400

    chore(player): remove unused imports

--- Redline/Assets/Scripts/Controllers/PlayerController.cs
+++ Redline/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
