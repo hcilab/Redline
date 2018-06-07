cd5c8dcd496657ff3803c81cab1d1a216f28959f
Jason Wuertz <jwuertz@unb.ca>
2018-06-07 00:16:43 -0300

    add level manager

---
+++ Redline/Assets/Scripts/Controllers/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	[SerializeField] private PlayerController _player;
+	[SerializeField] private FireSystemController _fireSystem;
+	private GameMaster _gameMaster;
+	
+	public PlayerController Player
+	{
+		get { return _player; }
+	}
+
+	public FireSystemController FireSystem
+	{
+		get { return _fireSystem; }
+	}
+
+	public GameMaster GameMaster
+	{
+		get { return _gameMaster; }
+	}
+	
+	// Use this for initialization
+	void Start ()
+	{
+		SceneManager.sceneLoaded += InitializeLevel;
+	}
+
+	private void InitializeLevel( Scene arg0, LoadSceneMode arg1 )
+	{
+		_gameMaster = FindObjectOfType< GameMaster >();
+		
+		GameMaster.RegisterLevel( this );
+	}
+}
