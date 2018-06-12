69b1ef4aedd1ee8b96ef82d4728097332b6e9770
Jason Wuertz <jwuertz@unb.ca>
2018-06-12 20:13:19 -0300

    new build and loading bar shit

---
+++ Redline/Assets/Plugins/helpers.jslib
@@ -0,0 +1,5 @@
+mergeInto( LibraryManager.library, {
+  RemoveLoader: function() {
+    window.alert("Loading finished!");
+  }
+});
---
+++ RedlineDataServer/public/UnityProgress.js
@@ -0,0 +1,19 @@
+function UnityProgress(gameInstance, progress) {
+  if (!gameInstance.Module)
+    return;
+  if (!gameInstance.progress) {
+    gameInstance.progress = document.getElementById('loader');
+    gameInstance.progressBars = gameInstance.progress.children;
+    gameInstance.msg = document.createElement('p');
+    gameInstance.msg.id = "msg";
+    gameInstance.msg.textContent = "Loading Game";
+    gameInstance.progress.appendChild(gameInstance.msg);
+  }
+
+  for( i = 0; i < progress * gameInstance.progressBars.length; i++ ) {
+      gameInstance.progressBars[i].classList.add('loaded');
+  }
+
+  if (progress == 1)
+    gameInstance.msg.textContent = "Setting up game";
+}
