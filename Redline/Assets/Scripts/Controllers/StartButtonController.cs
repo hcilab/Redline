324cf39bde251a680beb38b74d8409709690d58d
Jason Wuertz <jwuertz@unb.ca>
2018-06-14 13:38:42 -0300

    add input validation to start button

--- Redline/Assets/Scripts/Controllers/StartButtonController.cs
+++ Redline/Assets/Scripts/Controllers/StartButtonController.cs
@@ -11,23 +11,39 @@ public class StartButtonController : MonoBehaviour, IPointerClickHandler
 	private Button _startButton;
 	private string _customLevel;
 	[SerializeField] private GameObject _levelSelectionField;
+	[SerializeField] private GameMaster _gameMaster;
 
 	private void Start()
 	{
+		_startButton = GetComponent< Button >();
 		_levelSelectionField.SetActive( false );
 	}
 
 	public void OnPointerClick( PointerEventData eventData )
 	{
-		if ( eventData.button == PointerEventData.InputButton.Left )
+		if ( _startButton.interactable && eventData.button == PointerEventData.InputButton.Left )
 		{
 			_customLevel = _levelSelectionField.GetComponent<InputField>().text;
-			FindObjectOfType< GameMaster >().StartGame( );
+			_gameMaster.NextLevel( _customLevel );
 		} else if ( eventData.button == PointerEventData.InputButton.Right )
 		{
 			Debug.Log( _levelSelectionField  );
+			_customLevel = _gameMaster.CurrentLevel.ToString();
+			_levelSelectionField.GetComponent< InputField >().text = _customLevel;
 			_levelSelectionField.SetActive( true );
 			_levelSelectionField.GetComponent<InputField>().ActivateInputField();
+			_startButton.interactable = true;
 		}
+	}
+
+	public void ValidateInput()
+	{
+		var lvlCount = _gameMaster.LevelCount;
+		var inputLvl = Int32.Parse( _levelSelectionField.GetComponent< InputField >().text );
+		if ( inputLvl <= 0 || inputLvl > lvlCount )
+		{
+			_startButton.interactable = false;
+		}
+		else _startButton.interactable = true;
 	}
 }
