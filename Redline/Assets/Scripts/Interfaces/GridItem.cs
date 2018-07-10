97e91968136ea506a288ea9e89e7e3d3f292adb5
Jason Würtz <jwuertz@upei.ca>
2018-07-10 12:15:26 -0300

    Revert "feat(GridItem): get neighbours now considers circular neighbourhood"
    This reverts commit 71d0a599d6c5f7d4d943899267c52f42d31637cb.

--- Redline/Assets/Scripts/Interfaces/GridItem.cs
+++ Redline/Assets/Scripts/Interfaces/GridItem.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using JetBrains.Annotations;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -193,40 +191,32 @@ public class GridItem
         return SetVariable( key, _variableSetters[key](this) );
     }
 
-    public GridItem[] GetNeighbours( int distance, [CanBeNull] GridItem subject = null )
+    public GridItem[] GetNeighbours( int distance )
     {
-        if ( subject == null ) subject = this;
-        
         List<GridItem> neighbours = new List<GridItem>();
-        float x = subject._gridCoords.x;
-        float y = subject._gridCoords.y;
+        float x = _gridCoords.x;
+        float y = _gridCoords.y;
 
-        //Check left
-        if (_gridCoords.y >= 1 )
-            neighbours.Add( _parentGrid.GetGridItem( x, y - 1 ) );
-    
-        //check up
-        if (_gridCoords.x >= 1 )
-            neighbours.Add( _parentGrid.GetGridItem( x - 1, y ) );
-    
-        //check right
-        if (_gridCoords.y < _parentGrid._cols - 1 - 1) 
-            neighbours.Add( _parentGrid.GetGridItem( x, y + 1 ) );
-    
-        //check down
-        if( _gridCoords.x < _parentGrid._rows - 1 - 1)
-            neighbours.Add( _parentGrid.GetGridItem( x + 1,y ) );
-
-        if ( distance == 1 ) return neighbours.ToArray();
-        else
+        for ( var i = 1; i <= distance; i++ )
         {
-            var newNeighbourhood = new List< GridItem >( neighbours );
-            foreach ( GridItem neighbour in neighbours )
-            {
-                newNeighbourhood = newNeighbourhood.Union( GetNeighbours( distance - 1, neighbour ) ).ToList();
-            }
-            return newNeighbourhood.ToArray();
+            //Check left
+            if (_gridCoords.y >= i )
+                neighbours.Add( _parentGrid.GetGridItem( x, y - i ) );
+        
+            //check up
+            if (_gridCoords.x >= i )
+                neighbours.Add( _parentGrid.GetGridItem( x - i, y ) );
+        
+            //check right
+            if (_gridCoords.y < _parentGrid._cols - 1 - i) 
+                neighbours.Add( _parentGrid.GetGridItem( x, y + i ) );
+        
+            //check down
+            if( _gridCoords.x < _parentGrid._rows - 1 - i)
+                neighbours.Add( _parentGrid.GetGridItem( x + i,y ) );     
         }
+       
+        return neighbours.ToArray();
     }
 
     public bool HasActivePayloadElements()
