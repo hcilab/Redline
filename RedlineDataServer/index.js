126137241a7b6e322972263db7b0c1f0ffc682c0
Jason Wuertz <jwuertz@unb.ca>
2018-08-09 18:20:46 -0300

    convert counter to number

--- RedlineDataServer/index.js
+++ RedlineDataServer/index.js
@@ -13,7 +13,7 @@ const { render, json, status, header } = server.reply;
 var redline_entry_schema =  mongoose.Schema({
     date: { type: Date, default: Date.now }
   , time: String
-  , counter: String
+  , counter: Number
   , mturk_id: Number
   , id: { type: Number }
   , trial: Number
@@ -54,7 +54,7 @@ const cors = server.utils.modern(corsExpress);
 const comp = server.utils.modern(compression);
 
 server(
-  { 
+  {
     security: {
       csrf: false
     },
